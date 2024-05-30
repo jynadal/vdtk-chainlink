c7219effa5a54ff8c5a0aa3d128c2c35b93c1cb4
jynadal <nadaljeanyves@hotmail.fr>
2024-05-30 16:08:18 +0200

    commit before first deploy

---
+++ src/consts.ts
@@ -0,0 +1,8 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'VDTK';
+export const SITE_DESCRIPTION = 'Welcome to VDTK All your movies in one app!';
+export const USER_NICKNAME = 'JohnJohn';
+export const USER_DESCRIPTION = 'a director';
+export const TRANSITION_API = true;
